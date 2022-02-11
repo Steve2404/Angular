7b694f307eb750e994d2d80081fabcb70f3509fd
Steve <jatsasteve@gmail.com>
2022-02-11 13:29:34 +0100

    Update 3 images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FaceSnap} from "./Model/face-snap.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+  mySnap!: FaceSnap;
+  mySnap2!: FaceSnap;
+  mySnap3!: FaceSnap;
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+        "Archibald",
+        "Mon meilleur ami depuis tout petit !",
+          new Date(),
+        0,
+        "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg"
+        );
+    this.mySnap2 = new FaceSnap(
+        "Tulipe-rose",
+        "je dévoile un amour naissant, un sentiment pur et fragile",
+          new Date(),
+          0,
+          "/assets/Images/tulipe-rose.jpg"
+    );
+    this.mySnap3 = new FaceSnap(
+        "Orchidee",
+        "j incarne  la beauté suprême, la splendeur  et la sensualité",
+        new Date(),
+        0,
+        "/assets/Images/Orchidee.jpg"
+    );
+  }
 }
