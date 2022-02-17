5816c0fda10f95cf513d19f5988db48347be33a3
Steve <jatsasteve@gmail.com>
2022-02-17 08:34:45 +0100

    Formatage de la Date:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {registerLocaleData} from "@angular/common";
+import * as fr from '@angular/common/locales/fr'; // Pour importer le francais
+import * as de from '@angular/common/locales/de'; // Pour importer l allemand
+
 
 import { AppComponent } from './app.component';
 import { FaceSnapeComponent } from './face-snape/face-snape.component';
@@ -12,7 +16,16 @@ import { FaceSnapeComponent } from './face-snape/face-snape.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'de-DE'
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(de.default)
+  }
+}
