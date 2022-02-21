9db3f85337c6d8198e6c5c084f14f6d2599e1568
Steve <jatsasteve@gmail.com>
2022-02-21 21:14:51 +0100

    Implementation d'une SPA(single Page Application)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,20 @@ import { AppComponent } from './app.component';
 import { FaceSnapeComponent } from './face-snape/face-snape.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapeComponent,
     FaceSnapListComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      AppRoutingModule
   ],
   providers: [
     {
